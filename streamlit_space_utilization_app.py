9d1acb375a73a600848b72605966ce4ebe7bddf0
Jidapa Buranachan <jidapa.bur@gmail.com>
2025-07-30 13:07:29 +0700

    Update streamlit_space_utilization_app.py

--- streamlit_space_utilization_app.py
+++ streamlit_space_utilization_app.py
@@ -49,7 +49,7 @@ if soh_file:
 #----------------------------------------------------------------------
     
     # Zone & dept config
-    zone_area = {1: 2520, 2: 3300, 3: 480}
+    zone_area = {1: 2520, 2: 3360, 3: 480}
     zone_stack_limit = {1: 3, 2: 1, 3: 1}
     dept_area = {"REFRIGERATOR": 1060, "TKB": 200, "WASHING MACHINE": 670, "T.V.": 590}
     dept_stack_limit = {"T.V.": 2.2, "REFRIGERATOR": 2.2, "WASHING MACHINE": 3, "TKB": 2.5}
