b4988bf6b3b4cf8c4ceb100785d6274980d377f6
Jidapa Buranachan <jidapa.bur@gmail.com>
2025-08-06 11:20:53 +0700

    Update streamlit_space_utilization_app.py

--- streamlit_space_utilization_app.py
+++ streamlit_space_utilization_app.py
@@ -49,7 +49,7 @@ if soh_file:
 #----------------------------------------------------------------------
     
     # Zone & dept config
-    zone_area = {1: 2800, 2: 2880, 3: 480}
+    zone_area = {1: 3300, 2: 2880, 3: 480}
     zone_stack_limit = {1: 3, 2: 1, 3: 1}
     dept_area = {"REFRIGERATOR": 1060, "TKB": 200, "WASHING MACHINE": 670, "T.V.": 590}
     dept_stack_limit = {"T.V.": 2.2, "REFRIGERATOR": 2.2, "WASHING MACHINE": 3, "TKB": 2.5}
@@ -59,7 +59,7 @@ if soh_file:
     dept_capacity = {}
     for dept, area in dept_area.items():
         usable_area = area * 0.90
-        stack = dept_stack_limit.get(dept, 3)
+        stack = dept_stack_limit.get(dept, 1.3)
         capacity = (usable_area / pallet_area) * stack
         dept_capacity[dept] = round(capacity)
 
